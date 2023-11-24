7e86e2d728861ebcb18eea9a48c1206601d643e1
vi <letrieuvi1209@gmail.com>
2023-11-24 10:42:38 +0700

    Bai Tap Lon

--- FRONTEND/admin/src/views/NotFoundPage.vue
+++ FRONTEND/admin/src/views/NotFoundPage.vue
@@ -1,9 +1,8 @@
 <template>
-	<div class="container">
-		<!-- <h1>404: Không tìm thấy trang.</h1> -->
-		<router-link class="parent" to="/admin">
+	<div class="page-wrap">
+		<router-link class="parent" to="/dashboard">
 			<h5 class="child">Quay trở lại trang chủ!</h5>
-			<img src="https://cdn.dribbble.com/users/6060852/screenshots/14344119/media/1a534174372706cc7988a4615b3fbbd7.jpg?compress=1&resize=1000x750&vertical=top" class="w-100" alt="">
+			<img src="https://sitechecker.pro/wp-content/uploads/2023/06/404-status-code.png" class="w-100" alt="">
         </router-link>
 	</div>
 </template>
@@ -28,5 +27,9 @@ export default {
 		position: absolute;
 		left: 40%;
     	margin-top: 20px;
+	}
+
+	img {
+		margin-top: -30px;
 	}
 </style>
